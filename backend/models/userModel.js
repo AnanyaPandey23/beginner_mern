15260b8eeae516042957a24295c4381edb1db4d4
Ananya <apandey2_be20@thapar.edu>
2023-08-04 11:29:50 +0530

    Commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+//create scheme
+const userSchema = new mongoose.Schema({
+    name : {
+        type:String,
+        required: true,
+    },
+    email : {
+        type:String,
+        unique:true,
+        required:true,
+    },
+    age : {
+        type:Number,
+    },
+},{timestamps: true});
+
+//create model 1
+const User = mongoose.model('User',userSchema)
+module.exports = User;
